cccaad558aa287ae34e0bf386f86807b542c2e0e
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-05-08 20:24:10 +0530

    lets start

---
+++ src/main/java/arrays/UniqueCharacter.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class UniqueCharacter {
+    /**
+     * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+     */
+
+    public static void main(String[] args) {
+    
+    }
+
+}
