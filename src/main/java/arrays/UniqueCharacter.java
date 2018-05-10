990adaaf56509efa6ee87124f5d3f270deaf51cb
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-05-10 20:43:24 +0530

    fix(Arrays): practice arrays

--- src/main/java/arrays/UniqueCharacter.java
+++ src/main/java/arrays/UniqueCharacter.java
@@ -1,12 +1,42 @@
 package arrays;
 
-public class UniqueCharacter {
-    /**
-     * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
-     */
+import java.util.Objects;
 
-    public static void main(String[] args) {
-    
-    }
+public class UniqueCharacter {
+	/**
+	 * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+	 */
+
+	public static void main(String[] args) {
+		UniqueCharacter uniqueCharacter = new UniqueCharacter();
+		String uniqueStr = "abcdef1234@#$";
+		System.out.println("Must be Unique : " + uniqueCharacter.isUnique(uniqueStr));
+		String notUnique = "abcc12345@";
+		System.out.println("Must be not Unique : " + uniqueCharacter.isUnique(notUnique));
+
+
+	}
+
+	private boolean isUnique(String str) {
+		Objects.requireNonNull(str, "input str must not be null");
+		// total supported charater is 128.. if more than 128 then must be repetated.
+		if (str.length() > 128) {
+			return false;
+		}
+		boolean[] chars = new boolean[128];
+
+		// store each char int value .. if encounter again return false.
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i);
+			if (chars[val]) {
+				return false;
+			} else {
+				chars[val] = true;
+			}
+		}
+
+		return true;
+
+	}
 
 }
